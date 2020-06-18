db5c945d5c25051e3306921a03a4842d43ed46ea
Christian Würker <kriss@ceusmedia.de>
2020-06-18 18:35:54 +0200

    Add first demo.

--- demo/parse.php
+++ demo/parse.php
@@ -2,17 +2,11 @@
 require_once __DIR__.'/../vendor/autoload.php';
 new UI_DevOutput;
 
-
-$f	= new CeusMedia\PhpParser\Structure\File_();
-print_m( $f );die;
-
-
-
-
 use CeusMedia\PhpParser\Parser\Regular as Parser;
 
 $path	= __DIR__.'/../src/';
 $parser	= new Parser();
 $result	= $parser->parseFile( $path.'Structure/Parameter.php', $path );
 
-print_m( $result );
+remark( 'Methods:' );
+print_m( current( $result->getClasses() )->getMethods() );
